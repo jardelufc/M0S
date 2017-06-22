82c2e750d1f321497a256f58c3c63042889d6de4
Madson Ivens Holanda Batista <madson.holanda@hotmail.com>
2017-06-22 15:16:28 -0300

    Cmodel update

--- cmodel/main.c
+++ cmodel/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TOTAL_MEMORY 16000; //Valor variável
+#define KERNEL_MEMORY_START 32;
+#define MAX_KERNEL_MEMORY 512;
+#define KERNEL_STACK_SIZE 256;
+#define IDLE_STACK_SIZE 256;
+#define FREE_MEMORY_START 1024;
+#define FREE_MEMORY_END 15488;
+#define idle_task 0;
 #define MAX_TASKS 4;
 #define MAX-MUTEX 8;
 #define TASK-STACK-SIZE 128;
@@ -38,6 +46,8 @@ typedef struct{
     uint32_t sys_timer;
     uint32_t stack_pos;
     uint32_t task_array;
+    uint32_t idle_stack_pos;
+    uint32_t entry_nr_sched;
     uint32_t mutex_storage;
 }ofs;
 
@@ -45,10 +55,10 @@ typedef struct{
    // uint8_t unescheduled;
 }estado;
 
-void escalonador(int r2, int r3, int r4, int r5, int r6){
+void scheduler(uint32_t r2, uint32_t r3, uint32_t r4, uint32_t r5, uint32_t r6){
     ofs of;
-    int r0=0;   //main start
-    int r1= of.sys_timer;
+    uint32_t r0=0;   //main start
+    uint32_t r1= of.sys_timer;
     r1+=1;
     of.sys_timer=r1;
 
@@ -59,7 +69,7 @@ void escalonador(int r2, int r3, int r4, int r5, int r6){
     r10=r4;
     r11=r5;
 
-    int r6 = of.current_task;
+    r6 = of.current_task;
     if(r6 <0)
         continue_normal_tasks();
     of.stack_pos=r1;
@@ -149,11 +159,88 @@ void  find_next_task(uint32_t r6, uint32_t r2, uint32_t r0, uint32_t r1){
     //ands r6,r2 pesquisar mais acerca, aparentemente representa &&, mas o código não apresenta uma condição.
 
     r2 = r2+r0;
-    r2 = r2+ofs.task_array;
+    r2 = r2+ofs.task_array;   //r2 se torna o endereço da próxima tarefa
 
+    r1 = r2 + task.entry_state;
+    if(r1 == RUNNING)
+        task_found();
+    if(r1== UNSCHEDULED)
+        it_is_an_unscheduled_task();
+
+    r5--;
+    if(r5!=0)
+        find_next_task();
 }
 
-void
+void schedule_idle_task(uint32_t r1, uint32_t r2, uint32_t r0){
+    r1 = r0 + ofs.entry_nr_sched;
+    r1++;
+    ofs.entry_nr_sched=r1;
+
+    r1 =  r0 + ofs.current_task;
+    r2 = 1;
+    r2 = r2 + 32; // igual a lsls r2,31 ? linha 219
+    r1 = r2;     // igual a orrs r1,r2 ?
+    ofs.current_task = r2;
+
+    r0 = r0+ofs.idle_stack_pos;
+    r1 = idle_task+1; //linha 224, idle_stack é um bloco, contudo não há retorno de valor da função
+    Frame.PC=r1;
+    restore_stack();
+}
+
+void it_is_an_unscheduled_task(uint32_t r3, uint32_t r1, uint32_t r2){
+    r3 = 1;
+    r1 = RUNNING;
+    r1 = r2 +task.entry_state;
+}
+
+void task_found(uint32_t r1, uint32_t r6, uint32_t r2, uint32_t r3, uint32_t r0){
+    r1 = r6;
+    ofs.current_task = r1;
+
+    r1 = r2 + task.entry_nr_sched;
+    r1++;
+    task.entry_nr_sched = r1;
+
+    r1 = r2 + task.entry_nr_sched;
+    //msr PSP, r1, aparentemente não aplicável em C, uma vez q solicita o uso de um  coprocessador
+
+    if(r3!=0)
+        scheduler_exit();
+}
+
+void restore_stack(uint32_t r0, uint32_t r4, uint32_t r7, uint32_t r1, uint32_t r2, uint32_t r3, uint32_t r8,uint32_t r9,uint32_t r10, uint32_t r11){
+    r1 = r0;
+    r2 = r0 + 0x04;
+    r3 = r0 + 0x08;
+    r8 = r1;
+    r9 = r2;
+    r10 = r3;
+    r1 = r0+0x012;
+    r11 = r1;
+
+    r0 =  r0 +0x10;
+    //msr PSP,r0;
+}
+
+void scheduler_exit(){
+    setbuf(stdout, 0);  // pode ser usador no lugar de isb ?
+    scheduler(uint32_t r2, uint32_t r3, uint32_t r4, uint32_t r5, uint32_t r6);
+}
+
+//----------------------------------------------------------------------------------
+// Para o próximo bloco, tem-se que, o  Idle task será escalonado no caso de nenhuma outra
+//tarefa estar disponível para escalonamento.
+// Tentará concatenar dois blocos de memória livre diferentes e então entrar em  loop.
+// Também pode ser usado para colocar o sistema em repouso visando economizar energia.
+//----------------------------------------------------------------------------------
+
+void idle_task(){
+    r5 =  FREE_MEMORY_START; //FREE_MEMORY_START = KERNEL_MEMORY_START + MAX_KERNEL_MEMORY +KERNEL_STACK_SIZE+IDLE_TASK_STACK_SIZE
+    r4 = FREE_MEMORY_END;//FREE_MEMORY_END = KERNEL_MEMORY_START + TOTAL MEMORY-(MAX_TASKS*TASk_STACK_SIZE)
+    r0 = 0;
+}
 
 int main()
 {
